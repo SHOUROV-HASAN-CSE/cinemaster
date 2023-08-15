d1a0a3ef3a098708cee40bb00ed29589e5684898
Shourov Hasan <shourovhasancse@gmail.com>
2023-08-16 01:14:47 +0600

    some component create

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='h-header bg-main w-full'>
+     <h1 className="text-h1 text-subMain font-bold underline"> CINEMASTER</h1>
     </div>
   );
-}
+};
 
 export default App;
