9ba28959b4e31dfe44a5a1fc68587f7a2f0b5a14
Shourov Hasan <shourovhasancse@gmail.com>
2023-11-22 23:29:18 +0600

    another route create

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import NotFound from './Screens/NotFound';
 import ContactUs from './Screens/ContactUs';
 import MoviesPage from './Screens/Movies';
 import SingleMovie from './Screens/SingleMovie';
+import WatchPage from './Screens/WatchPage';
+import Login from './Screens/Login';
+import Register from './Screens/Register';
 
 const App = () => {
   return (
@@ -14,7 +17,10 @@ const App = () => {
       <Route path='/about-us' element={<AboutUs/>}/>
       <Route path='/contact-us' element={<ContactUs/>}/>
       <Route path='/movies' element={<MoviesPage/>}/>
-      <Route path='/movies/:id' element={<SingleMovie/>}/>
+      <Route path='/movie/:id' element={<SingleMovie/>}/>
+      <Route path='/watch/:id' element={<WatchPage/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/register' element={<Register/>}/>
       <Route path='*' element={<NotFound/>}/>
     </Routes>
   );
--- src/Components/UsedInputs.js
+++ src/Components/UsedInputs.js
@@ -19,4 +19,18 @@ export const Select = ({ label, options, onChange }) =>{
     </select>
     </>
   )
-}
+};
+export const Input = ({ label, placeholder, type, bg }) =>{
+  return(
+    <div className="text-sm w-full">
+    <label className="text-border font-semibold">{label}</label>
+    <input
+    required
+    type={type}
+    placeholder={placeholder} 
+    className={`w-full mt-2 p-5 text-sm border border-border rounded text-white ${
+      bg ? 'bg-main' : 'bg-dry'} `}
+    />
+    </div>
+  )
+};
